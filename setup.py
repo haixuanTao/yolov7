39c17fb724b996ec2baaf47a9e118c3cf6c5c8fc
haixuanTao <tao.xavier@outlook.com>
2022-09-26 21:15:47 +0200

    remove packages from `setup.py`

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import setuptools
 
-setuptools.setup(
+from setuptools import setup
+
+setup(
     name="yolov7_tt100k",
     version="0.0.0.dev0",
-    packages=["yolov7_tt100k"],
     package_dir={
         "yolov7_tt100k": ".",
     },
