47d1cf108648a00c672c38e924af7d91407253a7
haixuanTao <tao.xavier@outlook.com>
2022-09-29 18:32:16 +0200

    make demo simpler

--- demo.py
+++ demo.py
@@ -4,12 +4,17 @@ import cv2
 import numpy as np
 import torch
 
-from yolov7_tt100k import (WEIGHTS, letterbox, non_max_suppression,
-                           plot_one_box, scale_coords)
+from yolov7_tt100k import (
+    WEIGHTS,
+    letterbox,
+    non_max_suppression,
+    plot_one_box,
+    scale_coords,
+)
 
 model = torch.load(WEIGHTS)  # load
-model = model['ema' if model.get('ema') else 'model'].float().fuse().eval()
-image = os.path.join(os.path.dirname(__file__),"2.jpg")
+model = model["ema" if model.get("ema") else "model"].float().fuse().eval()
+image = os.path.join(os.path.dirname(__file__), "2.jpg")
 
 img = cv2.imread(image)
 im0 = cv2.imread(image)
@@ -23,13 +28,12 @@ if img.ndimension() == 3:
     img = img.unsqueeze(0)
 
 pred = model(img)[0]
-pred = non_max_suppression(pred, 0.15, 0.15, None, False)
+pred = non_max_suppression(pred, 0.15, 0.15, None, False)[0]
 
-for i, det in enumerate(pred):  # detections per image
-    # Rescale boxes from img_size to im0 size
-    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-    for *xyxy, conf, cls in reversed(det):
-            plot_one_box(xyxy, im0, label="res", color=int(cls), line_thickness=1)
+pred[:, :4] = scale_coords(img.shape[2:], pred[:, :4], im0.shape).round()
+for *xyxy, conf, cls in reversed(pred):
+    plot_one_box(xyxy, im0, label="res", color=int(cls), line_thickness=1)
+print(pred.detach().cpu().numpy())
 
 cv2.imshow(str(1), im0)
-cv2.waitKey(1)  # 1 millisecond
+cv2.waitKey(10000)  # 1 millisecond
